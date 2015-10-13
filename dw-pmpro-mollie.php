e7817c1a4227b997d9dabc0f00dad98f13bde7e1
Daan Wilmer <daan@daanwilmer.nl>
2015-10-13 20:47:30 +0200

    Added plugin base file

---
+++ dw-pmpro-mollie.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: PMPro Mollie Payment Gateway
+ * Version: 0.1
+ * Description: A Paid Memberships Pro plugin to add a payment gateway for Mollie
+ * Author: Daan Wilmer
+ * Author URL: http://daanwilmer.nl/
+ * License: GPLv2
+ */
+// todo: code the thing
